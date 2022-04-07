dcd605fe62c1a41e6c98f355693e646e1840a591
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-04-06 20:02:50 -0400

    conditional rendering of components and navbar update

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -9,8 +9,24 @@ function ContactForm() {
   const { name, email, message } = formState;
 
   function handleChange(e) {
-    setFormState({ ...formState, [e.target.name]: e.target.value})
-  }
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Email is invalid');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if(!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required`)
+      } else {
+        setErrorMessage('')
+      }
+    }
+    if(!errorMessage) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+
+  }}
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -23,17 +39,22 @@ function ContactForm() {
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+          <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" defaultValue={email} name="email" onChange={handleChange} />
+          <input type="email" defaultValue={email} name="email" oonBlurnChange={handleChange} />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+          <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />
         </div>
-        <button type = 'submit'>Submit</button>
+        {errorMessage && (
+          <div>
+            <p className='error-text'>{errorMessage}</p>
+          </div>
+        )}
+        <button type="submit">Submit</button>
       </form>
     </section>
   );
